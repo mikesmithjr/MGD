45bf9511f7b37f428d7a9d4defcc10039792aaab
mikesmithjr <mikesmithjr@fullsail.edu>
2014-02-18 23:32:39 -0600

    Inital commit after having to restore my entire system.  Thanks Mavericks!!!

---
+++ Week 3/WolfRun/WolfRun/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WolfRun
+//
+//  Created by Michael Smith on 2/6/14.
+//  Copyright Michael Smith 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
